64523566d317579179a6b27ae05131e787ea9415
Carlos_Parra_19 <56228034+carlosparramartinez@users.noreply.github.com>
2023-09-07 20:57:09 -0500

    Add files via upload

---
+++ frontend_movil_1/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using frontend_movil_1.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace frontend_movil_1.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
